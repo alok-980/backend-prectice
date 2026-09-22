abd45bab924bcf5f2883ae1077593b3eb0ce089b
Alok Chauhan <alokchauhan7896@gmail.com>
2026-09-22 15:42:32 +0530

    product - model, and route is created

--- Day_22_23_snitch/src/app/app.js
+++ Day_22_23_snitch/src/app/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoute from '../routes/auth.route.js';
+import productRoute from '../routes/product.route.js';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoute);
+app.use('/api/product', productRoute);
 
 export default app;
